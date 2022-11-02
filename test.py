25183dc52224acee61fd410d33c110ae24cc4b90
Ian Thomson <it18aaa@herts.ac.uk>
2022-11-02 10:45:58 +0000

    initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2 as cv
+import util
+
+
+def main():
+    print('test')
+    d = util.Thing("d")
+    p = util.Thing("p")
+    print(p.name())
+    print(d.name())
+
+if __name__ == "__main__":
+    main()
