e43a82c1ec93cf50d67af6e603bf0bba6b05990e
Ian Thomson <it18aaa@herts.ac.uk>
2022-11-02 11:50:14 +0000

    deleted rubbish

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
 import numpy as np
 import cv2 as cv
-import util
 
 
-def main():
-    print('test')
-    d = util.Thing("d")
-    p = util.Thing("p")
-    print(p.name())
-    print(d.name())
-
-if __name__ == "__main__":
-    main()
+print('hello world')
